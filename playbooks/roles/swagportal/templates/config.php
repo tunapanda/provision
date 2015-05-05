af3b7456059d003de0d27297312b811bd16bc0f6
Brad Smith <brad@geekdome.net>
2015-05-05 12:44:47 -0400

    make learninlocker credentials variables

--- playbooks/roles/swagportal/templates/config.php
+++ playbooks/roles/swagportal/templates/config.php
@@ -1,8 +1,8 @@
 <?php
 
 	$xapiEndpoint="http://learninglocker.{{provision__fqdn}}/data/xAPI/";
-	$xapiUsername="b47fc60e6d92b4fa5497b7c38bcb2b98606fa5ae";
-	$xapiPassword="a6485b033734f630f9af5a9ac732ec10db5d92b7";
+	$xapiUsername="{{learninglocker__xapi_user}}";
+	$xapiPassword="{{learninglocker__xapi_pass}}";
 	$actorDomain="{{provision__fqdn}}";
 	$useProxy=FALSE;
 
