be0b684de10266267b3bc41dec5544b5312e2edb
Brad Smith <brad@geekdome.net>
2015-05-06 18:55:49 -0400

    fix config format, extension

--- playbooks/roles/swagportal/templates/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-	$xapiEndpoint="http://learninglocker.{{provision__fqdn}}/data/xAPI/";
-	$xapiUsername="{{learninglocker__xapi_user}}";
-	$xapiPassword="{{learninglocker__xapi_pass}}";
-	$actorDomain="{{provision__fqdn}}";
-	$useProxy=FALSE;
-
